b72822613c71956746a5c8bff972bf87d9e62dea
RasinduNHA <IT22092410@my.sliit.lk>
2025-05-07 23:05:57 +0530

    feat:Plan entity

---
+++ Recipe/src/main/java/com/example/Recipe/entity/Plan.java
@@ -0,0 +1,25 @@
+package com.example.springmongoproject.Model;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Builder
+@Document(collection = "plan")
+@Getter
+public class Plan {
+    @Id
+    String planId;
+    String planTitle;
+    String planMainIngredients;
+    String planDescription;
+    String planStartDate;
+    String planEndDate;
+    String planDifficulty;
+    String planCategory;
+
+
+}
